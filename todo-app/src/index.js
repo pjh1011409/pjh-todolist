f923ecccdf2ce375d8858c57395064776441b05e
ParkJeongHo <pjh31208@naver.com>
2022-10-18 11:49:46 +0900

    빈페이지 출력

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -7,10 +7,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode
-    basename="/
-  wanted-pre-onboarding-frontend"
-  >
+  <React.StrictMode basename={process.env.PUBLIC_URL}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
