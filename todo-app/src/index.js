159797c6a1a77b7f4c2dcdfa437afa4513296905
ParkJeongHo <pjh31208@naver.com>
2022-10-18 16:24:14 +0900

    basename명 에러

--- todo-app/src/index.js
+++ todo-app/src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.BASE_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
